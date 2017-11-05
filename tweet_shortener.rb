2240eeae03c8bfc8a15e7c0fb8356d395d9ad1ec
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 14:18:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,24 +1,14 @@
 # Write your code here.
 def dictionary
 
-dictionary = {"hello" => "hi", 
-  "to" => '2', 
-  "two" => '2', 
-  "too" => '2', 
-  "for" => '4', 
-  "four" => '4', 
-  "be" => "b", 
-  "you" => "u", 
+dictionary = {"hello" => "hi",
+  "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  "be" => "b",
+  "you" => "u",
   "at" => "@",
   "and" => "&"}
 end
-
-  #```
-  # "hello" becomes 'hi'
-  # "to, two, too" become '2' 
-  # "for, four" become '4'
-  # 'be' becomes 'b'
-  # 'you' becomes 'u'
-  # "at" becomes "@" 
-  # "and" becomes "&"
-  # ```
