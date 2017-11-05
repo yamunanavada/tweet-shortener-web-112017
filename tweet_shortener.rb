b99f514d85cd2fb2072a9e9cab63a47619c9931b
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 14:47:16 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -55,7 +55,7 @@ def shortened_tweet_truncator(tweet)
   new_tweet = selective_tweet_shortener(tweet)
 
   if new_tweet.length > 140
-    truncated_tweet = new_tweet[0..137] + "..."
+    truncated_tweet = new_tweet[0...137] + "..."
   else
     new_tweet
   end
