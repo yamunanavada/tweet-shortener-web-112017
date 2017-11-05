59c6311d69c6256dc7502fc09b776b6d427425d5
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 14:35:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,3 +32,12 @@ def word_substituter(string)
   new_string = new_string_array.join(" ")
   new_string
 end
+
+def bulk_tweet_shortener(array)
+
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+
+
+end
